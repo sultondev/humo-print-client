7771fe14a63363c5a86eb36f78a141cc473643c9
sultondev <sultondeveloper@gmail.com>
2026-04-23 00:06:13 +0500

    fix: initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="">
+        <UHeader title="HUMO PRINT UZ" />
+         <UMain>
+           <UContainer>
+               <slot />
+           </UContainer>
+        </UMain>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const appConfig = useAppConfig() // { foo: 'bar' }
+
+const newAppConfig = { foo: 'baz' }
+
+updateAppConfig(newAppConfig)
+
+console.log(appConfig) // { foo: 'baz' }
+</script>
+
+<template>
+    <div class="">
+        <h1 class="text-4xl font-bold">Humo print</h1>
+    </div>
+</template>
