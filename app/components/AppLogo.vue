3b841f1b368b93398798478d59bb524d89cc577b
sultondev <sultondeveloper@gmail.com>
2026-05-12 21:50:40 +0500

    fix: update logo

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -1,14 +1,15 @@
 <template>
   <NuxtLink to="/" class="flex items-center gap-2 cursor-pointer">
-    <div class="w-8 h-8 rounded-lg bg-accent flex items-center justify-center flex-shrink-0">
+    <!-- <div class="w-8 h-8 rounded-lg">
       <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
         <rect x="2" y="5" width="14" height="9" rx="1.5" stroke="white" stroke-width="1.5" />
         <rect x="5" y="2" width="8" height="3" rx="1" fill="white" />
         <circle cx="13" cy="9.5" r="1" fill="white" />
       </svg>
-    </div>
+    </div> -->
+    <NuxtImg src="/logo.png" class=" flex items-center justify-center w-8 h-8 " alt="DBprint Logo" />
     <span class="font-outfit font-bold text-xl text-white tracking-tight">
-      humo<span class="text-accent">print</span>
+      DB<span class="text-accent">print</span>
     </span>
   </NuxtLink>
 </template>
